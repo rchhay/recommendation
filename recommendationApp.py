1a0fa6176c2ea057031377f989f76c3dc1c0f7c7
Rathanak Chhay <rathanak.chhay@gmail.com>
2025-10-23 22:33:05 -0400

    add web search

--- recommendationApp.py
+++ recommendationApp.py
@@ -6,6 +6,7 @@ from langgraph.checkpoint.memory import MemorySaver
 from langchain.agents import create_agent
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
+from langchain_tavily import TavilySearch
 
 
 load_dotenv()
@@ -19,6 +20,10 @@ vector_store = FAISS.load_local(
 memory = MemorySaver()
 model = init_chat_model("gpt-4.1-nano")
 
+search = TavilySearch(max_results=2)
+tools = [
+    search
+]
 
 system_prompt = """
 You are a salesperson for WicksByWerby, a store that sells anime and video themed aromatics. 
@@ -26,9 +31,13 @@ You are a salesperson for WicksByWerby, a store that sells anime and video theme
 Your role is to recommend products based on the products in the store. Do not hallucinate products that are not available in the store. 
 
 You should specify the product by the TITLE. Provide up to 3 recommendations if possible. 
+If you do not know the answer, then you can use the internet to the WicksByWerby etsy store. You are only allow to search the following site:
+https://www.etsy.com/shop/WicksByWerby
+
+If you do not know the answer, it is ok to reply that you do not know the answer or that there is no answer. 
 """
 
-agent_executor = create_agent(model, system_prompt=system_prompt, checkpointer=memory)
+agent_executor = create_agent(model, tools=tools, system_prompt=system_prompt, checkpointer=memory)
 
 # Use the agent
 config = {"configurable": {"thread_id": "abc123"}}
@@ -46,15 +55,7 @@ def ask():
     retrieved_docs = vector_store.similarity_search(question)
     docs_content = "\n\n".join(doc.page_content for doc in retrieved_docs)
 
-    # input_message = {
-    #     "role": "user",
-    #     "content": question,
-    # }
     response = ""
-
-
-    print("====================== docs content ===============")
-    print(docs_content)
 
 
     for step in agent_executor.stream(
